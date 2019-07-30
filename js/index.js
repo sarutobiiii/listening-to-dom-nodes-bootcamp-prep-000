f7f033a647d62fea799a56f6bfb10bced52d5023
oluwatobi balogun <tobibags19@gmail.com>
2019-07-30 02:16:01 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -20,11 +20,11 @@ input.addEventListener('keydown', function(e) {
 let divs = document.querySelectorAll('div');
  
 function bubble(e) {
-  console.log(this.firstChild.nodeValue.trim() + ' bubbled');
+ console.log(this.firstChild.nodeValue.trim() + ' bubbled');
 }
  
 for (let i = 0; i < divs.length; i++) {
-  divs[i].addEventListener('click', bubble);
+ divs[i].addEventListener('click', bubble);
 }
 
 From <https://learn.co/tracks/bootcamp-prep/javascript-fundamentals/the-dom/listening-to-nodes> 
